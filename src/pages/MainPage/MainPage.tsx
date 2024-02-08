2452056745fc2b6bc7525e229c804c4b5c3d63b1
SergioGDev <admin@sergiogdev.com>
2024-02-08 21:12:28 +0100

    feat: added description message

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,11 +3,16 @@ import FileLoaderWidget from "src/widgets/FileLoaderWidget/FileLoaderWidget";
 import DocumentCanvas from "src/components/document/DocumentCanvas/DocumentCanvas";
 
 const MainPage = () => {
-
   return (
     <div className={styles.container}>
       <h1>Mobbeel SGH</h1>
-
+      <div className={styles.description}>
+        Our advanced web app utilizes image recognition to extract and display
+        precisely cropped document images. Seamlessly processing documents from
+        attachments, photos, or real-time videos, our user-friendly solution
+        streamlines the document handling process with efficiency and precision,
+        eliminating the need for manual cropping.
+      </div>
       <FileLoaderWidget />
       <DocumentCanvas />
     </div>
